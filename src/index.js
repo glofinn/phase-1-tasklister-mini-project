fe243b76d5dbc382dffae539880591e4dcc3bf89
Finn Chen <finn.chen.maenhaut@gmail.com>
2023-02-14 10:40:27 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.querySelector("#create-task-form");
+const tasks = document.querySelector("#tasks");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e.target["new-task-description"].value);
+  let newTask = document.createElement("li");
+  newTask.textContent = e.target["new-task-description"].value;
+  tasks.append(newTask);
 });
